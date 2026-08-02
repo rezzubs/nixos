d38f4dbb6634d104759483361949a42bea0211d0
Marten Roots; <marten.roots@gmail.com>
2026-08-02 22:14:39 +0300

    feat: Add bulk data disk

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -13,6 +13,17 @@
     "/".options = ["compress=zstd"];
     "/home".options = ["compress=zstd"];
     "/nix/store".options = ["compress=zstd" "noatime"];
+
+    "/mnt/data0" = {
+      device = "/dev/disk/by-uuid/fda2eadb-817c-48c5-b6f9-fb69085c6433";
+      fsType = "btrfs";
+      options = [
+        "users" # Allows any user to mount/unmount
+        "nofail" # Allows system to continue to boot if drive cannot be mounted
+        "exec" # Allows execution of files
+        "compress=zstd"
+      ];
+    };
   };
 
   networking.hostName = "desktop";
