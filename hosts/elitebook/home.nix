bc6b8726fefb13fc3a1204937e3c013f8fc2f83a
Marten Roots; <marten.roots@gmail.com>
2026-08-06 16:59:08 +0300

    feat(elitebook): Add niri output config

--- hosts/elitebook/home.nix
+++ hosts/elitebook/home.nix
@@ -1,6 +1,8 @@
 {...}: {
   imports = [../../home];
 
+  custom.niri.hostConfig = ./niri.kdl;
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
